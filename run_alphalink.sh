e96508ba6ad029814bd1743365c63c89ee5a6238
carloschacon-cell <carloschacon2121@gmail.com>
2025-08-06 12:12:44 +0200

    changin the databases paths to fit our settings

--- run_alphalink.sh
+++ run_alphalink.sh
@@ -10,24 +10,22 @@ neff=${9:--1}
 dropout_crosslinks=${10:--1}
 
 echo "Starting MSA generation..."
-python unifold/homo_search.py \
+python /apps/AlphaLink/AlphaLink2/unifold/homo_search.py \
     --fasta_path=$fasta_path \
     --max_template_date=$max_template_date \
     --output_dir=$output_dir_base  \
-    --uniref90_database_path=$database_dir/uniref90/uniref90.fasta \
-    --mgnify_database_path=$database_dir/mgnify/mgy_clusters_2018_12.fa \
-    --bfd_database_path=$database_dir/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
-    --uniclust30_database_path=$database_dir/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
-    --uniprot_database_path=$database_dir/uniprot/uniprot.fasta \
-    --pdb_seqres_database_path=$database_dir/pdb_seqres/pdb_seqres.txt \
-    --template_mmcif_dir=$database_dir/pdb_mmcif/mmcif_files \
-    --obsolete_pdbs_path=$database_dir/pdb_mmcif/obsolete.dat \
+    --uniref90_database_path=$database_dir/uniref90_2022_05.fasta \
+    --mgnify_database_path=$database_dir/mgy_clusters_2022_05.fa \
+    --bfd_database_path=$database_dir/bfd-first_non_consensus_sequences.fasta \
+    --uniprot_database_path=$database_dir/uniprot_all_2021_04.fasta \
+    --pdb_seqres_database_path=$database_dir/pdb_seqres_2022_09_28.fasta \
+    --template_mmcif_dir=$database_dir/mmcif_files \
     --use_precomputed_msas=True
 
 echo "Starting prediction..."
 fasta_file=$(basename $fasta_path)
 target_name=${fasta_file%.fa*}
-python inference.py \
+python /apps/AlphaLink/AlphaLink2/inference.py \
 	--model_name="multimer_af2_crop" \
 	--param_path=$param_path \
 	--data_dir=$output_dir_base \
